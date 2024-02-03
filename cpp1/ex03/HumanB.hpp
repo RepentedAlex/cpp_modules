29de027665d81884a17e567f015f3f43bc5b0259
Alessandro Petitcollin <petitcollin.alessandro@gmail.com>
2024-02-03 14:31:31 +0100

    CPP01/ex02 : HumanB.hpp is complete

---
+++ cpp1/ex03/HumanB.hpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   HumanB.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: apetitco <apetitco@sutdent.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/03 14:22:11 by apetitco          #+#    #+#             */
+/*   Updated: 2024/02/03 14:29:37 by apetitco         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HUMANB_HPP
+#define HUMANB_HPP
+
+#include "Weapon.hpp"
+#include "iostream"
+
+class HumanB
+{
+private:
+	std::string	_name;
+	Weapon		*_weapon;
+public:
+	HumanB(std::string name);
+	~HumanB(void);
+	void	attack(void);
+	void	setWeapon(Weapon &weapon);
+};
+
+#endif
