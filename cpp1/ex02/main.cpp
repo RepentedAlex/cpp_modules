433b14b96c7c3ab518bde97dec534b661f440cd5
Alessandro Petitcollin <petitcollin.alessandro@gmail.com>
2024-02-03 13:27:23 +0100

    CPP01/ex02 : DONE

---
+++ cpp1/ex02/main.cpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: apetitco <apetitco@sutdent.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/03 13:04:49 by apetitco          #+#    #+#             */
+/*   Updated: 2024/02/03 13:23:28 by apetitco         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int	main(void)
+{
+	std::string	string;
+	std::string	*stringPTR;
+
+	string = "HI THIS IS BRAIN";
+	stringPTR = &string;
+	std::string	&stringREF = string;
+	std::cout << "string address: " << &string << std::endl;
+	std::cout << "stringPTR address: " << stringPTR << std::endl;
+	std::cout << "stringREF address: " << &stringREF << std::endl;
+	std::cout << "string value: " << string << std::endl;
+	std::cout << "stringPTR value: " << *stringPTR << std::endl;
+	std::cout << "stringREF value: " << stringREF << std::endl;
+}
